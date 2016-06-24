3df20e3159bfdbcc537aa11b605b367fa8fb6a0a
LeParadoxHD <psgameplay97@gmail.com>
2016-06-24 22:41:13 +0200

    version 1.0

---
+++ Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace Vysor_Pro_Patcher
+{
+    partial class Principal
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Principal));
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.status = new System.Windows.Forms.ToolStripStatusLabel();
+            this.begin = new System.Windows.Forms.Button();
+            this.offline = new System.Windows.Forms.CheckBox();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.youtubeChannel = new System.Windows.Forms.LinkLabel();
+            this.source = new System.Windows.Forms.ComboBox();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.status});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 77);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(263, 22);
+            this.statusStrip1.TabIndex = 0;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // status
+            // 
+            this.status.Name = "status";
+            this.status.Size = new System.Drawing.Size(23, 17);
+            this.status.Text = "OK";
+            // 
+            // begin
+            // 
+            this.begin.BackColor = System.Drawing.Color.PaleGreen;
+            this.begin.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.begin.Location = new System.Drawing.Point(13, 13);
+            this.begin.Name = "begin";
+            this.begin.Size = new System.Drawing.Size(238, 35);
+            this.begin.TabIndex = 1;
+            this.begin.Text = "Patch Vysor Pro";
+            this.begin.UseVisualStyleBackColor = false;
+            this.begin.Click += new System.EventHandler(this.begin_Click);
+            // 
+            // offline
+            // 
+            this.offline.AutoSize = true;
+            this.offline.Location = new System.Drawing.Point(41, 54);
+            this.offline.Name = "offline";
+            this.offline.Size = new System.Drawing.Size(56, 17);
+            this.offline.TabIndex = 2;
+            this.offline.Text = "Offline";
+            this.offline.UseVisualStyleBackColor = true;
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.ToolTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            // 
+            // youtubeChannel
+            // 
+            this.youtubeChannel.AutoSize = true;
+            this.youtubeChannel.LinkBehavior = System.Windows.Forms.LinkBehavior.NeverUnderline;
+            this.youtubeChannel.Location = new System.Drawing.Point(155, 81);
+            this.youtubeChannel.Name = "youtubeChannel";
+            this.youtubeChannel.Size = new System.Drawing.Size(93, 13);
+            this.youtubeChannel.TabIndex = 3;
+            this.youtubeChannel.TabStop = true;
+            this.youtubeChannel.Text = "YouTube Channel";
+            this.youtubeChannel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // source
+            // 
+            this.source.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.source.FormattingEnabled = true;
+            this.source.Items.AddRange(new object[] {
+            "Default",
+            "ChromeDefaultData"});
+            this.source.Location = new System.Drawing.Point(103, 52);
+            this.source.Name = "source";
+            this.source.Size = new System.Drawing.Size(120, 21);
+            this.source.TabIndex = 4;
+            // 
+            // Principal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(263, 99);
+            this.Controls.Add(this.source);
+            this.Controls.Add(this.youtubeChannel);
+            this.Controls.Add(this.offline);
+            this.Controls.Add(this.begin);
+            this.Controls.Add(this.statusStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Principal";
+            this.Text = "Vysor Pro Patcher";
+            this.Shown += new System.EventHandler(this.Principal_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel status;
+        private System.Windows.Forms.Button begin;
+        private System.Windows.Forms.CheckBox offline;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.LinkLabel youtubeChannel;
+        private System.Windows.Forms.ComboBox source;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using System.Net;
+
+namespace Vysor_Pro_Patcher
+{
+    public partial class Principal : Form
+    {
+        string baseFolder;
+        string chromeExtensionsFolder;
+        string vysorFolder;
+
+        public static bool CheckInternet()
+        {
+            try
+            {
+                using (var client = new WebClient())
+                {
+                    using (var stream = client.OpenRead("http://www.google.com"))
+                    {
+                        return true;
+                    }
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public static bool TryVersion(string version)
+        {
+            try
+            {
+                using (var client = new WebClient())
+                {
+                    using (var stream = client.OpenRead("http://www.leparadoxhd.com/crack/vysor/" + version + "/uglify.js"))
+                    {
+                        return true;
+                    }
+                }
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        public Principal()
+        {
+            InitializeComponent();
+        }
+
+        private void Principal_Load(object sender, EventArgs e)
+        {
+            source.SelectedIndex = 0;
+            // ToolTip
+            ToolTip toolTipOffline = new ToolTip();
+            toolTipOffline.AutoPopDelay = 500000;
+            toolTipOffline.InitialDelay = 500;
+            toolTipOffline.ReshowDelay = 500;
+            toolTipOffline.ShowAlways = true;
+            toolTipOffline.SetToolTip(this.offline, "Si se activa, no se usará el archivo parcheado desde internet, se intentará parchear manualmente.");
+            status.Text = "Verifying Vysor...";
+            baseFolder = Environment.GetEnvironmentVariable("LocalAppData");
+            switch (source.Text)
+            {
+                case "Default":
+                    chromeExtensionsFolder = baseFolder + "\\Google\\Chrome\\User Data\\Default\\Extensions";
+                    break;
+                case "ChromeDefaultData":
+                    chromeExtensionsFolder = baseFolder + "\\Google\\Chrome\\User Data\\ChromeDefaultData\\Extensions";
+                    break;
+            }
+            vysorFolder = chromeExtensionsFolder + "\\gidgenkbbabolejbgbpnhbimgjbffefm";
+            if (!Directory.Exists(vysorFolder))
+            {
+                status.Text = "Vysor App Not Found :(";
+            } else
+            {
+                status.Text = "Verifying Vysor Version...";
+                var directory = new DirectoryInfo(vysorFolder);
+                var vysorVersion = directory.GetDirectories()
+                .OrderByDescending(f => f.LastWriteTime)
+                .First();
+                var version = vysorVersion.ToString().Substring(0, vysorVersion.ToString().Length - 2);
+                status.Text = "Vysor: " + version;
+            }
+        }
+
+        private void Paso1()
+        {
+            status.Text = "Verifying Vysor Version...";
+            var directory = new DirectoryInfo(vysorFolder);
+            var vysorVersion = directory.GetDirectories()
+            .OrderByDescending(f => f.LastWriteTime)
+            .First();
+            var version = vysorVersion.ToString().Substring(0, vysorVersion.ToString().Length - 2);
+            status.Text = "Vysor: " + version;
+            while (!CheckInternet())
+            {
+                DialogResult internet = MessageBox.Show("Network Connection not found :(", "Error", MessageBoxButtons.RetryCancel, MessageBoxIcon.Error);
+                if (internet == DialogResult.Cancel)
+                {
+                    System.Environment.Exit(1);
+                }
+                if (CheckInternet())
+                {
+                    break;
+                }
+            }
+
+            if (TryVersion(version))
+            {
+                if (!offline.Checked)
+                {
+                    if (File.Exists(vysorFolder + "\\" + version + "_0" + "\\uglify.js"))
+                    {
+                        File.Delete(vysorFolder + "\\" + version + "_0" + "\\uglify.js");
+                    }
+                }
+                Directory.SetCurrentDirectory(vysorFolder + "\\" + version + "_0");
+                if ( offline.Checked )
+                {
+                    string text = File.ReadAllText("uglify.js");
+                    text = text.Replace("this.licensed=false", "this.licensed=true");
+                    text = text.Replace("this.licenseCached=false", "this.licenseCached=true");
+                    File.WriteAllText("uglify.js", text);
+                } else
+                {
+                    WebClient uglify = new WebClient();
+                    uglify.DownloadFile("http://www.leparadoxhd.com/crack/vysor/" + version + "/uglify.js", "uglify.js");
+                }
+                status.Text = "¡Patched!";
+                Process vysor = Process.Start("chrome.exe", "--app-id=gidgenkbbabolejbgbpnhbimgjbffefm");
+                Thread.Sleep(2000);
+                if ( vysor != null )
+                {
+                    Process[] vysorPid = Process.GetProcessesByName("chrome");
+                    foreach ( Process pid in vysorPid )
+                    {
+                        pid.Kill();
+                    }
+                }
+                Process.Start("chrome.exe", "--app-id=gidgenkbbabolejbgbpnhbimgjbffefm");
+            } else
+            {
+                MessageBox.Show("I still have not done a crack :(", "Almost There", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                DialogResult abrirWeb = MessageBox.Show("Do you want to alert me a new version has been released?", 
+                    "Leave me a comment", MessageBoxButtons.YesNo, MessageBoxIcon.Information);
+                if ( abrirWeb == DialogResult.Yes )
+                {
+                    Process.Start("https://www.youtube.com/watch?v=G36c-_BdoME#comment-section-renderer");
+                }
+            }
+        }
+
+        private void begin_Click(object sender, EventArgs e)
+        {
+            if (!Directory.Exists(vysorFolder))
+            {
+                status.Text = "Vysor App Not Found.";
+            }
+            else
+            {
+                Paso1();
+            }
+        }
+
+        
+    }
+}
